de45b116b816ccb525840456e3e202b7d51566e7
yogesh <ypc@gmail.com>
2025-06-10 17:02:44 +0530

    quick fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,6 +34,14 @@ resource "aws_security_group" "hritikmania_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Allow port 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
