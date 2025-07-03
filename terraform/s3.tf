0745b82ead43b7b3f95024261af908e2e038d4c4
yogesh <ypc@gmail.com>
2025-07-03 16:52:47 +0530

    resetup infa and test

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -11,8 +11,8 @@ resource "aws_s3_bucket" "media_bucket" {
 resource "aws_s3_bucket_public_access_block" "media_bucket_block" {
   bucket = aws_s3_bucket.media_bucket.id
 
-  block_public_acls       = true
-  ignore_public_acls      = true
+  block_public_acls       = false
+  ignore_public_acls      = false
   block_public_policy     = false
   restrict_public_buckets = false
 }
